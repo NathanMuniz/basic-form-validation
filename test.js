4e29d07b998af70ecb28b594e2296d6347c368e1
NathanMuniz <natangabriel.palmeiras@gmail.com>
2022-11-11 14:11:59 -0300

    algumas alterações

---
+++ test.js
@@ -0,0 +1,17 @@
+function fator(n) {
+  var cont = 1;
+  for (var i = n; i > 0; i--) {
+
+    result = cont * i
+    cont = result
+
+
+  }
+  return result
+
+}
+
+console.log(fator(50))
+
+
+// An = (n // 2) * 1 
